c1bfd08ef9d3afc9fff89d15fdeb22b154a99262
Alan Huang <cesium@mit.edu>
2010-05-16 19:30:14 -0400

    Maybe personals.

--- zephyr_daemon.py
+++ zephyr_daemon.py
@@ -7,24 +7,25 @@ import Zephyr
 
 URL = 'http://ennesby.appspot.com/zephyr'
 try:
-	name, waveid = sys.argv[:2]
-except ValueError:
-	sys.exit('Usage: python zephyr_daemon.py <url> <class...>')
+	waveid = sys.argv[1]
+	personal = sys.argv[2] == '-p'
+except IndexError:
+	sys.exit('Usage: python zephyr_daemon.py <url> [-p] <class...>')
+if personal:
+	del sys.argv[2]
 
-def loop():
-	zobject = Zephyr.Zephyr()
-	for zclass in sys.argv[2:]:
-		zobject.subscribe((zclass, '*', '*'))
+zobject = Zephyr.Zephyr()
+for zclass in sys.argv[2:]:
+	zobject.subscribe((zclass, '*', '*'))
+if not personal:
 	zobject.unsubscribe(('message', 'personal', zobject.sender()))
 	zobject.unsubscribe(('message', 'urgent', zobject.sender()))
-	while 1:
-		select.select([zobject.getfd()], [], [])
+print 'Subbed to:', zobject.subscribe()
+while 1:
+	select.select([zobject.getfd()], [], [])
+	msg = zobject.check()
+	while msg:
+		del msg['zephyr']
+		print urlopen(URL, urlencode({ 'waveid' : waveid, 'body' : simplejson.dumps(msg) })).read()
+		print '%s: Processed zephyr on -c %s' % (msg['time'], msg['zclass'])
 		msg = zobject.check()
-		while msg:
-			del msg['zephyr']
-			print urlopen(URL, urlencode({ 'waveid' : waveid, 'body' : simplejson.dumps(msg) })).read()
-			print '%s: Processed zephyr on -c %s' % (msg['time'], msg['zclass'])
-			msg = zobject.check()
-
-if __name__ == '__main__':
-	loop()
